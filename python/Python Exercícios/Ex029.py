12c52d3990b514df84c0c1a77a4d68d1f64922bf
Samuel <samuelcampelo47@gmail.com>
2025-12-22 13:06:59 -0300

    Exercício sobre cores no terminal (Ex029)

--- python/Python Exercícios/Ex029.py
+++ python/Python Exercícios/Ex029.py
@@ -1,3 +1,7 @@
+print('\033[33m=+\033[m' * 20)
+print('\033[1;34mRADAR DE TRÂNSITO')
+print('\033[33m=+\033[m' * 20)
+
 velocidade = int(input('Qual a velocidade do carro? '))
 if velocidade > 80:
     print(f'Vc excedeu o limite de velocidade! Vc tem que pagar {(velocidade-80)*7} reais de multa.')
